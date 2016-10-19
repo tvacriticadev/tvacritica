66ea9971aa67c1cafabbf664ffd902fc474dc521
Patric <administrador@imac-ti.local>
2016-10-19 11:01:26 -0400

    Tentando criar controle de audio na lock screen

--- Acritica FM 93/ViewController.swift
+++ Acritica FM 93/ViewController.swift
@@ -22,15 +22,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UIApplication.shared.beginReceivingRemoteControlEvents()
+        
+        if NSClassFromString("MPNowPlayingInfoCenter") != nil {
+           // let image:UIImage = UIImage(named: "logo_player_background")! // comment this if you don't use an image
+            //let albumArt = MPMediaItemArtwork(image: image) // comment this if you don't use an image
+            let songInfo = [
+                MPMediaItemPropertyTitle: "Radio A Crítica",
+                MPMediaItemPropertyArtist: "93,1fm",
+               //MPMediaItemPropertyArtwork: albumArt // comment this if you don't use an image
+            ] as [String : Any]
+            
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = (songInfo as NSObject) as! [String : Any]
+        }
         //TRY CATCH PARA PLAY EM BACKBROUND
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            UIApplication.shared.beginReceivingRemoteControlEvents()
-            print("Receiving remote control events\n")
-        } catch {
-            print("Audio Session error.\n")
+            print("AVAudioSession Category Playback OK")
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+                print("AVAudioSession is Active")
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
         }
-       
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
